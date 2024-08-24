b343b88c3107862e32f316abd624973cab7b63d1
nazarageenko <nazarageenko@gmail.com>
2024-08-24 07:22:21 +0300

    added SpringBoot skeleton

---
+++ src/main/java/com/github/nazarageenko/GramToGram/GramToGramApplication.java
@@ -0,0 +1,13 @@
+package com.github.nazarageenko.GramToGram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramToGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GramToGramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nazarageenko/GramToGram/GramToGramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nazarageenko.GramToGram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramToGramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
